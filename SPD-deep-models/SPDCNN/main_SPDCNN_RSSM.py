48e8e3ebf3160e0c3a4c822d7769b4a3f59aab00
lalalaGegrx <93378195+lalalaGegrx@users.noreply.github.com>
2025-03-17 15:29:59 +0800

    Update main_SPDCNN_RSSM.py

--- SPD-deep-models/SPDCNN/main_SPDCNN_RSSM.py
+++ SPD-deep-models/SPDCNN/main_SPDCNN_RSSM.py
@@ -138,5 +138,3 @@ if __name__ == "__main__":
     mf1_mean = sum(mf1_l) / args.subjects
     print(sum(acc_l) / args.subjects)
     print(sum(mf1_l) / args.subjects)
-
-    sio.savemat('/data/lalalagegrx/RiemannianRNN/EcoGLibrary/SEED/Results/SPDCNN/SPDCNN_RSSM_10fold_shuffle_results.mat', {'acc':acc_l, 'mf1': mf1_l})
